9efe60bccb25ef63660d47bdca029776f93d1381
나영철 <4e65rv@feelway.com>
2020-08-21 18:25:20 +0900

    init: blog files

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,13 +1,7 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
+        "title": "testtest!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
         "url": "http://localhost:4000/jekyll/update/post/",
-        "teaser": null
-      },{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["B329.github.io","jekyll","update"],
-        "tags": [],
-        "url": "http://localhost:4000/b329.github.io/jekyll/update/welcome-to-jekyll/",
         "teaser": null
       }]
