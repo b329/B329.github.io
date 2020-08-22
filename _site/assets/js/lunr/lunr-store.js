1342ec2edb314d21b46925ebd6d0ac095f56cf2f
나영철 <4e65rv@feelway.com>
2020-08-22 18:59:13 +0900

    add: first blog files

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "springboot-2.2 Init 세팅에 대한 기록",
-        "excerpt":"기본 springframework 을 만들면서 기록을 함께 해두기 위해 작성되었음. 아래 소르 링크를 참조하면 기본적으로 제작한 springboot 2.2.0 을 기반으로 작성한 소스코드를 내려받고 실행해볼 수 있습니다. ​ branch 에 대한 설명 develop_1 : 기본 framework 까지 develop_2 : mustache 를 이용한 화면 구성까지 향후 목표는 이 기본 framework 에 restAPI 를...","categories": ["springboot","기술블로그"],
+        "title": "@Autowired 가 없는 이유",
+        "excerpt":"@Autowired 가 없는 이유는 빈을 주입받는 방식에 @Autowired setter 생성자 주입방식이 있는데 여기서는 Controller 와 Service 에서 lombok 의 @RequiredArgsConstructor 로 생성자 Bean 을 주입받기 때문에 Autowired 가 없다. 생성자를 직접 안쓰고 lombok 을 사용하는 이점은 해당 클래스의 의존성 관계가 변경될때마다 생성자코드를 계속해서 수정해야 하는 번거로움을 피하기 위해서이다. ​ @RequiredArgsConstructor...","categories": ["springboot","기술블로그"],
         "tags": [],
         "url": "http://localhost:4000/springboot/%EA%B8%B0%EC%88%A0%EB%B8%94%EB%A1%9C%EA%B7%B8/post/",
         "teaser": null
