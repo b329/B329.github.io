866df8f60cadc49419443ba51eb886f3f0f399ba
나영철 <4e65rv@feelway.com>
2020-08-22 18:48:15 +0900

    add: firat blog files

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "testtest!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "title": "springboot-2.2 Init 세팅에 대한 기록",
+        "excerpt":"기본 springframework 을 만들면서 기록을 함께 해두기 위해 작성되었음. 아래 소르 링크를 참조하면 기본적으로 제작한 springboot 2.2.0 을 기반으로 작성한 소스코드를 내려받고 실행해볼 수 있습니다. ​ branch 에 대한 설명 develop_1 : 기본 framework 까지 develop_2 : mustache 를 이용한 화면 구성까지 향후 목표는 이 기본 framework 에 restAPI 를...","categories": ["springboot","기술블로그"],
         "tags": [],
-        "url": "http://localhost:4000/jekyll/update/post/",
+        "url": "http://localhost:4000/springboot/%EA%B8%B0%EC%88%A0%EB%B8%94%EB%A1%9C%EA%B7%B8/post/",
         "teaser": null
       }]
