3496623195d7e48897c297e42dea4be5896f1510
나영철 <4e65rv@feelway.com>
2020-08-23 15:30:43 +0900

    fix: _config.yml

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -10,4 +10,10 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/springboot/post/",
         "teaser": "http://localhost:4000/assets/images/500x300.png"
+      },{
+        "title": "springboot-2.2 Init 세팅에 대한 기록",
+        "excerpt":"기본 springframework 을 만들면서 기록을 함께 해두기 위해 작성되었음. 아래 소스 링크를 참조하면 기본적으로 제작한 springboot 2.2.0 을 기반으로 작성한 소스코드를 내려받고 실행해볼 수 있습니다. ​ branch 에 대한 설명 develop_1 : 기본 framework 까지 develop_2 : mustache 를 이용한 화면 구성까지 향후 목표는 이 기본 framework 에 restAPI 를...","categories": ["kafka"],
+        "tags": [],
+        "url": "http://localhost:4000/kafka/kafka/",
+        "teaser": "http://localhost:4000/assets/images/500x300.png"
       }]
