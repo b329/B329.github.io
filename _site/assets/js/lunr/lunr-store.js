bca6e2ce1e23a931dfa39e92a6ef89e37bf6f658
나영철 <4e65rv@feelway.com>
2020-09-09 11:38:53 +0900

    add: agile

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -53,6 +53,12 @@ var store = [{
         "url": "http://localhost:4000/portfolio/2020-08-24-kafka_do_2/",
         "teaser": null
       },{
+        "title": "Agile & Scrum 에 대한 정리",
+        "excerpt":"-왜 기민한(agile) 조직이 되야 하는가? 기업간 경쟁이 치열해지는 상황에서 언제 나타날지 모르는 잠재적 경쟁자들과 싸워야 하며, 달라지는 고객 및 조직원들의 요구에도 기민하게 반응해야 살아남을 수 있는 환경이 되었다. 숨겨져 있던 고객의 니즈를 발굴하여 짧은 시간 안에 글로벌 기업으로 자리잡은 예) 페이스북, 우버, 에어이앤비 전문가들은 위계적인 구조에서 리더가 지시, 통제하는 기존의...","categories": ["scrum"],
+        "tags": ["scrum"],
+        "url": "http://localhost:4000/portfolio/2020-09-08-scrum/",
+        "teaser": null
+      },{
         "title": "@Autowired 가 없는 이유",
         "excerpt":"@Autowired 가 없는 이유는 빈을 주입받는 방식에 @Autowired setter 생성자 주입방식이 있는데 여기서는 Controller 와 Service 에서 lombok 의 @RequiredArgsConstructor 로 생성자 Bean 을 주입받기 때문에 Autowired 가 없다. 생성자를 직접 안쓰고 lombok 을 사용하는 이점은 해당 클래스의 의존성 관계가 변경될때마다 생성자코드를 계속해서 수정해야 하는 번거로움을 피하기 위해서이다. @RequiredArgsConstructor @Service...","categories": ["springboot"],
         "tags": ["springboot"],
@@ -87,5 +93,11 @@ var store = [{
         "excerpt":"-Producer 메세지 생산하기 $ bin/kafka-console-producer.sh --broker-list 127.0.0.1:9092, 127.0.0.1:9093, 127.0.0.1:9094 --topic PayLog &gt;login buyer &gt;searching product &gt;finding Tods &gt;pay price &gt;completed buying &gt;logout &gt; -Consumer 메세지 소비하기 $ bin/kafka-console-consumer.sh --bootstrap-server 127.0.0.1:9092, 127.0.0.1:9093, 127.0.0.1:9094 --topic PayLog --from-beginning b329-MacBookPro:kafka_2.13-2.6.0 b329$ bin/kafka-console-consumer.sh --bootstrap-server 127.0.0.1:9092, 127.0.0.1:9093, 127.0.0.1:9094 --topic PayLog --from-beginning login buyer searching product finding...","categories": ["kafka"],
         "tags": ["kafka"],
         "url": "http://localhost:4000/kafka/kafka_do_2/",
+        "teaser": null
+      },{
+        "title": "Agile & Scrum 에 대한 정리",
+        "excerpt":"-왜 기민한(agile) 조직이 되야 하는가? 기업간 경쟁이 치열해지는 상황에서 언제 나타날지 모르는 잠재적 경쟁자들과 싸워야 하며, 달라지는 고객 및 조직원들의 요구에도 기민하게 반응해야 살아남을 수 있는 환경이 되었다. 숨겨져 있던 고객의 니즈를 발굴하여 짧은 시간 안에 글로벌 기업으로 자리잡은 예) 페이스북, 우버, 에어이앤비 전문가들은 위계적인 구조에서 리더가 지시, 통제하는 기존의...","categories": ["scrum"],
+        "tags": ["scrum"],
+        "url": "http://localhost:4000/scrum/scrum/",
         "teaser": null
       }]
